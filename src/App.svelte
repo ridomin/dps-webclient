6d6f2f3df48eb7409f593a8bd2908db740a9b2be
rido-min <rido-min@users.noreply.github.com>
2020-11-09 00:38:21 -0800

    init dps web client

---
+++ src/App.svelte
@@ -0,0 +1,84 @@
+<script>
+	import {createHmac} from './hmac'
+	import {registerDevice} from './dpsClient'
+	export let scopeId = '0ne0015ABF4'
+	export let deviceId = 'testdps01'
+	export let deviceKey 
+	export let masterKey = 'gq4DZfE52zN5IvPIW0AVorT+QKHJA4SZVYLVWeQqzdYxbciKSEWs8oe+H8cb61YVP3qqxDOsVsmNOUBrcv7g6A=='
+	export let modelId = 'dtmi:com:example:Thermostat;1'
+
+	$: deviceKey = createHmac(masterKey, deviceId).then(d => deviceKey = d)
+
+	let dpsPromise
+
+	function provision() {
+		dpsPromise = registerDevice(scopeId, deviceId, deviceKey, modelId)
+	}
+</script>
+
+<main>
+	<h1>DPS WebClient</h1>
+
+	<p>
+		<label for="inputScopeId">Scope Id</label>
+		<input type="text" id="inputScopeId" bind:value={scopeId} />
+	</p>
+
+	<p>
+		<label for="inputDeviceId">DeviceId</label>
+		<input type="text" id="inputDeviceId" bind:value={deviceId}>
+	</p>
+	<p>
+		<label for="inputModelId">Model Id</label>
+		<input type="text" id="inputModelId" size="55" bind:value={modelId}>
+	</p>
+	<p>
+		<label for="inputDeviceKey">DeviceKey</label>
+		<input type="text" id="inputDeviceKey" size="55" bind:value={deviceKey}>
+	</p>
+	
+	<p>
+		<label for="inputMasterKey">use MasterKey
+			<input type="checkbox" >
+		</label>
+	
+		<input type="text" id="inputMasterKey" size="105" bind:value={masterKey}>
+	</p>
+	<p class="right">
+		<button on:click="{provision}" id="buttonProvision">Provision</button>
+	</p>
+	{#await dpsPromise}
+		<p>.. provisioning device ..</p>
+	{:then dpsResult}
+		<a href={dpsResult}>{dpsResult}</a>
+	{/await}
+
+</main>
+
+<style>
+	main {
+		text-align: left;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	label {
+		display: inline-block;
+		width: 140px;
+		text-align: right;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 2em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 800px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/dpsClient.js
@@ -0,0 +1,41 @@
+import { generateSasToken } from './hmac'
+
+export const registerDevice = async (scopeId, deviceId, deviceKey, modelId) => {
+  const endpoint = 'https://dps-client.azurewebsites.net/api/Provision'
+  const qs = encodeURIComponent(`ScopeId=${scopeId};DeviceId=${deviceId};SharedAccessKey=${deviceKey};ModelId=${modelId}`)
+  const url = `${endpoint}?DCF=${qs}`
+  console.log(url)
+  const resp = await fetch(url,
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Encoding': 'utf-8'
+      }
+    }
+  )
+  const response = await resp.text()
+  console.log(response)
+  return response
+}
+
+export const registerDeviceRest = async (scopeId, deviceId, deviceKey) => {
+  const url = `https://global.azure-devices-provisioning.net/${scopeId}/registrations/${deviceId}/register?api-version=2019-03-31`
+  const token = await generateSasToken(url, deviceKey, 'registration', 3600000)
+  console.log(token)
+  const resp = await fetch(url,
+    {
+      method: 'PUT',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Encoding': 'utf-8',
+        Authorization: token
+      },
+      redirect: 'follow',
+      body: JSON.stringify({ registrationId: deviceId })
+    }
+  )
+  const response = await resp.json()
+  console.log(response)
+}
